233b8c066bd509730225befba0f4afa126014376
Amrulla Khan Patan <amrulla-khan.patan@arup.com>
2018-10-12 14:35:08 +0530

    Notified openGL about the structure of each vertex

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -40,6 +40,9 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), &positions, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *)0);
+	glEnableVertexAttribArray(0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
