8f068a7d0af338e53e56d98613190bf53638d041
Amrulla Khan Patan <amrulla-khan.patan@arup.com>
2018-10-12 16:18:03 +0530

    Seperated shader strings to a seperate file and supporting methods

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filePath)
+{
+	enum ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	std::ifstream stream(filePath);
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+	std::string line;
+	
+	while (getline(stream, line))
+	{
+		if (line.find("#shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+			{
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos)
+			{
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else
+		{
+			if (type == ShaderType::NONE)
+			{
+				continue;
+			}
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return{ ss[0].str(), ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -85,27 +130,8 @@ int main(void)
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void *)0);
 	glEnableVertexAttribArray(0);
 
-	std::string vertextShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
-
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-		"}\n";
-
-	unsigned int shader = CreateShader(vertextShader, fragmentShader);
+	ShaderProgramSource sources = ParseShader("res/shaders/Basic.shader");
+	unsigned int shader = CreateShader(sources.VertexSource, sources.FragmentSource);
 	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
